e077d4970caa2fa91d8cb2d9cce1e2e4776e4513
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-16 18:38:17 +0530

    Done

--- assets/string.py
+++ assets/string.py
@@ -1,13 +1,99 @@
 import os
 
-ADMIN_TXT = 
-AFK_TEXT = 
-CARBON_TXT = 
-CONNECT_TXT = 
-FILTER_TXT = 
-GROUP_TXT = 
-LOCK_TXT = 
-MUSIC_TXT = 
+HELP_TXT = """
+𝙷𝙴𝚈 {mention}
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙼𝙴𝙽𝚄 𝙵𝙾𝚁 𝚈𝙾𝚄:
+"""
+
+ADMIN_TXT = """
+**𝙷𝚎𝚕𝚙 𝙵𝚘𝚛 𝙰𝚍𝚖𝚒𝚗 𝙼𝚘𝚍𝚞𝚕𝚎:**
+
+Make it easy to promote and demote with admin module!
+
+USAGE:
+➢ /promote - Promote an user as admin.
+➢ /promote [title] - Promote an user with title.
+➢ /spromote - Silently promote an user as admin.
+➢ /demote - Demote an promoted admin.
+➢ /sdemote - Silently demote an admin.
+➢ /fpromote - Promote an user with all rights.
+➢ /set_title - Set or change title of an admin.
+➢ /admincache - Force refresh the admins list.
+
+NOTE:
+• Innexia should have admin privillage.
+• Only admins can execute these in a chat.
+• These commands can be used only in group.
+""" 
+
+AFK_TEXT = """
+**𝙷𝚎𝚕𝚙 𝙵𝚘𝚛 𝙰𝚏𝚔 𝙼𝚘𝚍𝚞𝚕𝚎:**
+
+Away From Keyboard is to tell that you're not available!
+
+USAGE:
+➢ /afk - Mark yourself as afk.
+➢ /afk [reason] - Mark yourself as afk with reason.
+➢ brb [reason] - Same as the afk command, but not a command.
+
+NOTE:
+• Innexia should have admin privillage.
+• These commands can be used only in group.
+• These commands can be used by any group member.
+"""
+ 
+CARBON_TXT = """
+**𝙷𝚎𝚕𝚙 𝙵𝚘𝚛 𝙲𝚊𝚛𝚋𝚘𝚗 𝙼𝚘𝚍𝚞𝚕𝚎:**
+
+Beautify your code using carbon.now.sh!
+
+USAGE:
+➢ /carbon [text] - Create carbon from the given text.
+➢ /carbon [reply] - Create carbon from the replied text.
+➢ /carbon [text]|[colour] - Create carbon with custom colour.
+➢ /carbon [reply] [colour] - Create carbon with custom colour.
+
+NOTE:
+• Innexia should have admin privillage.
+• These commands works on both pm and group.
+• These commands can be used by any group member.
+"""
+
+CONNECT_TXT = """
+**𝙷𝚎𝚕𝚙 𝙵𝚘𝚛 𝙲𝚘𝚗𝚗𝚎𝚌𝚝 𝙼𝚘𝚍𝚞𝚕𝚎:**
+
+Connect bot to your PM to avoid spamming in groups!
+
+USAGE:
+➢ /connect - Connect a specific chat.
+➢ /disconnect - Disconnect from a chat.
+➢ /connections - View your all connections.
+
+NOTE:
+• Innexia should have admin privillage.
+• Only group admins can add a connection.
+• Send /connect for connect me to ur PM
+"""
+FILTER_TXT = """
+**𝙷𝚎𝚕𝚙 𝙵𝚘𝚛 𝙵𝚒𝚕𝚝𝚎𝚛 𝙼𝚘𝚍𝚞𝚕𝚎:**
+
+Manual Filter is the feature where users can set automated replies for a particular keyword and bot will respond whenever a keyword is found the message!
+
+USAGE:
+➢ /filter - Add a filter in the chat.
+➢ /filters - View all the filters of the chat.
+➢ /stop - Delete a specific filter in the chat.
+➢ /stopall - Delete all filters in the chat. (chat owner only)
+
+NOTE:
+• Innexia should have admin privillage.
+• Only admins can add filters in a chat.
+• Alert buttons have a limit of 64 characters.
+"""
+GROUP_TXT = """
+
+LOCK_TXT = """
+MUSIC_TXT = """
 NOTE_TXT = 
 RULE_TXT =
 TOOL_TXT = 
