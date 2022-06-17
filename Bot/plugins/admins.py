f64831cfea09e76e9c8daac290303b16141b508e
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-17 15:32:14 +0530

    Create admins.py

---
+++ Bot/plugins/admins.py
@@ -0,0 +1,18 @@
+ADMIN_TXT = """
+✘ Some people need to be publicly banned; spammers, annoyances, or just trolls.
+Make it easy to promote and demote users with the admin module!
+‣ /tban - Ban A User For Specific Time
+‣ /warn - Warn A User
+‣ /dwarn - Delete the replied message warning its sender
+‣ /rmwarns - Remove All Warning of A User
+‣ /warns - Show Warning Of A User
+‣ /promote - Promote A Member
+‣ /fullpromote - Promote A Member With All Rights
+‣ /demote - Demote A Member
+‣ /tmute - Mute A User For Specific Time
+‣ /ban_ghosts - Ban Deleted Accounts
+‣ /report | @admins | @admin - Report A Message To Admins.
+➥ you promote or demote an admin manually, 
+and innexia doesn't realise it immediately. This is because 
+to avoid spamming telegram servers, admin status is cached locally.
+"""
