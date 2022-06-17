0f294902ad333495865bd0e7d4515a69c3f91213
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-17 15:32:48 +0530

    Create afk.py

---
+++ Bot/plugins/afk.py
@@ -0,0 +1,9 @@
+AFK_TXT = """
+✘ Enables afk for your account
+✘description: When you are in afk if any one tags you then your bot will reply as he is offline.
+✘AFK mean away from keyboard.
+options: If you want AFK reason with reason.
+‣ usage: /afk <reason>
+‣ examples: /afk Let Me Sleep.
+‣ note: "Switches off AFK when you type back anything, anywhere. You can use #afk in message to continue in afk without breaking it.
+"""
